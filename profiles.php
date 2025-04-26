2b17232c2508fd83f41213acdb1a84e1a9ccd024
NirmaniKeerthirathna <nirmani.keerthirathna22@gmail.com>
2025-04-26 20:47:05 +0530

    Modifications - reviews UI improvements

---
+++ profiles.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Employee Profiles</title>
+  <link rel="stylesheet" href="profiles.css">
+</head>
+<body>
+
+  <?php include('../header/header.php'); ?>
+
+  <h1 class="page-heading">Rate Employees</h1>
+  <div class="profiles-container" id="profilesContainer"></div>
+
+  <script src="profiles.js"></script>
+
+  <div id="toast" class="toast"></div>
+</body>
+</html>
